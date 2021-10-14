9f261a390c13f8add994d059cc78c17d87b90762
glizzz_y <73669084+glixzzy@users.noreply.github.com>
2021-10-14 17:47:35 -0500

    x

--- global/py/24hr-token-adder.py
+++ global/py/24hr-token-adder.py
@@ -2,7 +2,7 @@ import datetime
 import requests
 import time
 
-authToken = "x" # add your blooket token in string
+authToken = "blooket token here"  # add your blooket token in string
 
 
 def getName(token):
@@ -11,7 +11,9 @@ def getName(token):
 
     return r.json()["name"]
 
+
 blooketName = getName(authToken)
+
 
 def getLastTokenDate():
     r = requests.get(f"https://api.blooket.com/api/users?name={blooketName}")
@@ -23,10 +25,11 @@ def getLastTokenDate():
 
     return (last_token_date, date_now, account_created_date)
 
+
 def getCreatedDate():
     r = requests.get(f"https://api.blooket.com/api/users?name={blooketName}")
     return datetime.datetime.strptime(r.json()["dateCreated"], "%Y-%m-%dT%H:%M:%SZ")
-    
+
 
 def addTokens():
     getData = getLastTokenDate()
@@ -48,13 +51,13 @@ def addTokens():
             print(e)
     elif getData[0].day != getData[1].day + 1:
         print(f"{blooketName} has already earned max tokens in the past 24 hours!")
-    elif (getCreatedDate().year, getCreatedDate().month, getCreatedDate().day)  == (getData[1].year, getData[1].month, getData[1].day):
+    elif (getCreatedDate().year, getCreatedDate().month, getCreatedDate().day) == (getData[1].year, getData[1].month, getData[1].day):
         try:
-            r = requests.put(request_url, request_data, headers={"authorization": authToken})
+            r = requests.put(request_url, request_data, headers={
+                             "authorization": authToken})
             print(f"Rewarded {blooketName} 500 tokens")
         except Exception as e:
             print(e)
-
 
 
 while True:
